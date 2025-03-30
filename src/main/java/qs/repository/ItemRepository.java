b71558ef9570f2b1d5f584494c989145c51edb25
Kanishka Gupta <162425328+Kanishka032@users.noreply.github.com>
2025-03-30 14:42:27 +0530

    Delete src/main/java directory

--- src/main/java/qs/repository/ItemRepository.java
+++
@@ -1,9 +0,0 @@
-package edu.qs.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import edu.qs.model.entity.Item;
-
-public interface ItemRepository extends JpaRepository<Item, Integer> {
-
-}
