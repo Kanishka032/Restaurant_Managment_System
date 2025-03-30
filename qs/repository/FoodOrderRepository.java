86127b56a3364aa14d819d5c7f167916ad09e915
Kanishka Gupta <162425328+Kanishka032@users.noreply.github.com>
2025-03-30 14:44:34 +0530

    Delete qs directory

--- qs/repository/FoodOrderRepository.java
+++
@@ -1,8 +0,0 @@
-package edu.qs.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import edu.qs.model.entity.FoodOrder;
-
-public interface FoodOrderRepository extends JpaRepository<FoodOrder, Integer> {
-}
